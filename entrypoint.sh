9d8c130476cb8a1596eaec16bae42818970b244e
Bhupesh-V <varshneybhupesh@gmail.com>
2022-03-15 20:27:48 +0530

    touchup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# echo "::set-output name=short_ref::${GITHUB_REF#refs/*/}"
-
 all_open_prs=$(gh pr list --base "${GITHUB_REF#refs/*/}" --json author,number)
 printf "%s\n" "PRs with base ${GITHUB_REF#refs/*/}: $all_open_prs"
 prs_count=$(echo "$all_open_prs" | jq length)
@@ -10,6 +8,6 @@ printf "%s\n" "There are currently $prs_count open PRs"
 for (( c=0; c<$prs_count; c++ )); do
     pr_id=$(echo "$all_open_prs" | jq .["$c"].number)
     author=$(echo "$all_open_prs" | jq .["$c"].author.login | tr -d '"')
-    echo "Author for PR $pr_id is $author"
+    printf "%s\n" "Author for PR $pr_id is $author"
     gh pr comment $pr_id --body "Hey @$author 👋🏽 friendly reminder to update your PR/branch because there was a recent commit ($(git rev-parse HEAD)) to the base branch"
 done
